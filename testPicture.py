b964c6dec0b1daa3553c22352d90d989e64ed029
Rob Daigle <robertdaigle1209@gmail.com>
2024-02-13 19:59:44 -0500

    Added testPicture.py script to test the camera

---
+++ testPicture.py
@@ -0,0 +1,11 @@
+#This is a basic script to be run onboard the Pi for taking a test picture
+
+from picamera2 import Picamera2, Preview
+import time
+picam2 = Picamera2()
+camera_config = picam2.create_still_configuration(main={"size": (1920, 1080)}, lores={"size": (640, 480)}, display="lores")
+picam2.configure(camera_config)
+picam2.start_preview(Preview.QTGL)
+picam2.start()
+time.sleep(2)
+picam2.capture_file("test.jpg")
