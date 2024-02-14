552074057a0898872dbc81f2e82e9bcee28784ec
Rob Daigle <robertdaigle1209@gmail.com>
2024-02-13 20:32:37 -0500

    Updated testPicture.py with focusing

--- testPicture.py
+++ testPicture.py
@@ -2,10 +2,12 @@
 import picamera2
 import picamera
 from picamera2 import Picamera2, Preview
+from libcamera import controls
 import time
 picam2 = Picamera2()
 camera_config = picam2.create_still_configuration(main={"size": (1920, 1080)}, lores={"size": (640, 480)}, display="lores")
 picam2.configure(camera_config)
+picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous})
 #picam2.start_preview(Preview.QTGL)
 picam2.start()
 time.sleep(2)
